188d02af86bea2d87ebad4d11b5917bca368e03c
iagoPzi <iagopzivitovski@gmail.com>
2022-05-22 00:04:14 -0300

    config server.js

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log('sss')  
+
+
+const express = require('express');
+const { resolve } = require('path');
+
+const app = express();
+
+app.use('/', 
+    express.static(
+        resolve(__dirname, '/dist')
+    ))
+
+app.listen(process.env.PORT || 3000, (err) => {
+    if (err) {return console.log(err)}
+
+    console.log('Tudo OK')
+})
